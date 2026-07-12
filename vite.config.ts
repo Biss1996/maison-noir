f31c91d0df3d3257fb20355519c23894a5a5b61a
Bismark <bettkipkoech45@gmail.com>
2026-07-13 02:22:55 +0300

    Convert project to standard Vite app

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
-
-export default config
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
